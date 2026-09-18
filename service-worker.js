4c6d62e2d215ba94c388b55ba89abd24019ffc5e
gruporotele-glitch <gruporotele@gmail.com>
2026-09-18 13:22:41 -0400

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,33 @@
+const CACHE_NAME = 'km-hora-frota-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  // Não intercepta chamadas ao Apps Script (precisam ir sempre à rede)
+  if (event.request.url.includes('script.google.com')) return;
+
+  event.respondWith(
+    caches.match(event.request).then((cached) => cached || fetch(event.request))
+  );
+});
